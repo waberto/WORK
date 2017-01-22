a4b5f6ff1d77e96d45fccd7102940dd347ab936b
waberto <waberi.houssein@gmail.com>
2017-01-22 01:51:59 +0100

    first commit

--- test.php
+++ test.php
@@ -1,3 +1,12 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Ma page web</title>
+	</head>
+	<body>
+		<h1>Ma page web</h1>
+		<p>Aujourd'hui nous somme le <?php echo date('d/m/Y h:i:s'); .>.</p>
+	</body>
+</html>
 
-echo '<h1> Bonjour à tous <h1>';
